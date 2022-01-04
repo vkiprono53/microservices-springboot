47f0ca51666f91717ba8210c1a3f181c999910c1
Vkiprono53 <kipronovincent53@gmail.com>
2022-01-04 16:30:54 +0300

    config-server

---
+++ config-server/src/test/java/com/vkiprono/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkiprono.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
